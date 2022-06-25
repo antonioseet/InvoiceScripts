5070b6687a235d6a71aa8319614ef089437feebb
Abarrer1 <Alberto.barrera@wsu.edu>
2022-06-25 00:43:41 -0700

    Directory selection completed.

---
+++ InvoiceToolsForm/MainWindow.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace InvoiceToolsForm
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.renameButton = new System.Windows.Forms.Button();
+            this.updateRecordButton = new System.Windows.Forms.Button();
+            this.pdfPrintButton = new System.Windows.Forms.Button();
+            this.emailButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.Location = new System.Drawing.Point(12, 12);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(204, 77);
+            this.directoryButton.TabIndex = 0;
+            this.directoryButton.Text = "Choose Directory";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.DirectoryButton_Click);
+            // 
+            // renameButton
+            // 
+            this.renameButton.Location = new System.Drawing.Point(12, 95);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(204, 77);
+            this.renameButton.TabIndex = 1;
+            this.renameButton.Text = "Rename All Invoices";
+            this.renameButton.UseVisualStyleBackColor = true;
+            this.renameButton.Click += new System.EventHandler(this.RenameButton_Click);
+            // 
+            // updateRecordButton
+            // 
+            this.updateRecordButton.Location = new System.Drawing.Point(12, 178);
+            this.updateRecordButton.Name = "updateRecordButton";
+            this.updateRecordButton.Size = new System.Drawing.Size(204, 77);
+            this.updateRecordButton.TabIndex = 2;
+            this.updateRecordButton.Text = "Update Records";
+            this.updateRecordButton.UseVisualStyleBackColor = true;
+            this.updateRecordButton.Click += new System.EventHandler(this.UpdateRecordButton_Click);
+            // 
+            // pdfPrintButton
+            // 
+            this.pdfPrintButton.Location = new System.Drawing.Point(12, 261);
+            this.pdfPrintButton.Name = "pdfPrintButton";
+            this.pdfPrintButton.Size = new System.Drawing.Size(204, 77);
+            this.pdfPrintButton.TabIndex = 3;
+            this.pdfPrintButton.Text = "Print To PDF";
+            this.pdfPrintButton.UseVisualStyleBackColor = true;
+            this.pdfPrintButton.Click += new System.EventHandler(this.PdfPrintButton_Click);
+            // 
+            // emailButton
+            // 
+            this.emailButton.Location = new System.Drawing.Point(12, 344);
+            this.emailButton.Name = "emailButton";
+            this.emailButton.Size = new System.Drawing.Size(204, 77);
+            this.emailButton.TabIndex = 4;
+            this.emailButton.Text = "Email to addresses";
+            this.emailButton.UseVisualStyleBackColor = true;
+            this.emailButton.Click += new System.EventHandler(this.EmailButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(6, 19);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(257, 401);
+            this.outputTextBox.TabIndex = 5;
+            this.outputTextBox.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.outputTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(519, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 426);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.emailButton);
+            this.Controls.Add(this.pdfPrintButton);
+            this.Controls.Add(this.updateRecordButton);
+            this.Controls.Add(this.renameButton);
+            this.Controls.Add(this.directoryButton);
+            this.Name = "MainWindow";
+            this.Text = "Invoice Tools";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button directoryButton;
+        private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.Button updateRecordButton;
+        private System.Windows.Forms.Button pdfPrintButton;
+        private System.Windows.Forms.Button emailButton;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ InvoiceToolsForm/MainWindow.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceToolsForm
+{
+    public partial class MainWindow : Form
+    {
+        private string selectedPath;
+        private string[] files;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Choose a working directory
+        private void DirectoryButton_Click(object sender, EventArgs e)
+        {
+            this.selectedPath = string.Empty;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            // Report the number of files found in directory.
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                this.selectedPath = fbd.SelectedPath;
+                this.files = Directory.GetFiles(this.selectedPath);
+                print("Files found: " + this.files.Length.ToString());
+                print("Directory changed to: " + this.selectedPath);
+            }
+
+            // Print each file in the directory. TODO: May not be needed later.
+            int count = 1;
+            foreach(string file in this.files){
+                string[] fileNameDir = file.Split('\\');
+                print(count + ". " + fileNameDir[fileNameDir.Length-1]);
+                count++;
+            }
+
+        }
+
+        // Process the xls files to find the invoice number, building address and first name of person that it should send to.
+        // Afterwards, close file if needed and rename it given the found attributes.
+        private void RenameButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateRecordButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PdfPrintButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmailButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void print(string s)
+        {
+            string prev = this.outputTextBox.Text;
+            prev += s + "\n";
+            this.outputTextBox.Text = prev;
+        }
+    }
+}
